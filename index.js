48bb51089d7cab35ff2aa9e91f3b0f02c7637538
Andy-Vasseur <andyvasseurpro@outlook.com>
2024-10-14 23:03:25 +0200

    :globe_with_meridians: repo init

---
+++ index.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const { default: OBSWebSocket } = require("obs-websocket-js");
+const obs = new OBSWebSocket();
+
+const cors = require("cors");
+
+const router = require("./app/routers/index");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static("./public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use(router);
+
+const connect = async () => {
+  try {
+    const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
+      `${process.env.OBS_HOST}:${process.env.OBS_PORT}`,
+      process.env.OBS_PASSWORD,
+      { rpcVersion: 1 }
+    );
+
+    return `Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`;
+  } catch (error) {
+    console.error("Failed to connect", error.code, error.message);
+  }
+};
+
+connect().then((value) => console.log(value));
+
+app.post("/change-scene", async (req, res) => {
+  const { sceneName } = req.body;
+
+  try {
+    await obs.call("SetCurrentProgramScene", { sceneName });
+    res.status(200).json({ message: `Scene changed to ${sceneName}` });
+  } catch (error) {
+    console.error("Failed to change scene", error);
+    res.status(500).json({ message: "Failed to change scene" });
+  }
+});
+
+const PORT = process.env.PORT || 3310;
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:PORT`);
+});
